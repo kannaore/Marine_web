b812c9cbea2e1ea4a5134fba68e9cf4ee0f7551a
kannaore <maybejjj78@g.kit.ac.kr>
2025-12-29 10:45:04 +0900

    fix: Vercel 빌드 오류 수정 - sanity 타입 및 의존성 추가

--- marine-web-app/src/sanity/lib/image.ts
+++ marine-web-app/src/sanity/lib/image.ts
@@ -1,6 +1,8 @@
 import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "./client";
+
+// SanityImageSource type definition
+type SanityImageSource = Parameters<ReturnType<typeof imageUrlBuilder>["image"]>[0];
 
 const builder = imageUrlBuilder(client);
 
